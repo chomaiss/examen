04f75c15f43ff78a527d6e679857bfe5e9660f0d
chomaiss <162386101+chomaiss@users.noreply.github.com>
2024-03-05 16:51:57 -0800

    Update index.php

--- index.php
+++ index.php
@@ -14,4 +14,23 @@ $nike = [
   "donde fue creado" => "Nueva York, y más en concreto -Harlem. ",
    "que tipo de tenis son" => "son zapatillas deportivas robustas y su suela presenta una plataforma elevada y curvada para brindar un buen soporte en el juego.",
 ];
+$adidas = *[
+//caracteristicas 
+    "modelo" => "Grand court",
+   "color" => "blanco y negro",
+  "precio" => "1399",
+    "talla" => "8.5 usa",
+  "año creado" => "18 de agosto de 1949",
+   "diseñador" => "Adi Dassler",
+  "tienda" => "adidas",
+  "color mas bendido" => "negro",
+  "donde fue creado" => "Herzogenaurach, una localidad situada en Baviera (Alemania). ",
+   "que tipo de tenis son" => "Estilo deportivo retro, horma clasica, cierre de cordones, Parte superior de piel sintética, Forro sintético, Suela de goma.",
+];
+
+
+
+
+   
+]
 echo json_encode($nike);
